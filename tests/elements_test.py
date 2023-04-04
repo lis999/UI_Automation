95bdde0b0f1c597829fd789a64bdb9037fe5e568
Sergo Li <sergo.li.seattle@gmail.com>
2023-04-03 17:22:05 -0700

    automation POM added

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,9 +1,17 @@
 import time
 
-from pages.base_page import BasePage
+from pages.elements_page import TextBoxPage
 
 
-def test(driver):
-    page = BasePage(driver, 'https://www.google.com/')
-    page.open()
-    time.sleep(3)
+class TestElements:
+    class TestTextBox:
+
+        def test_text_box(self, driver):
+            text_box_page = TextBoxPage(driver, "https://demoqa.com/text-box")
+            text_box_page.open()
+            full_name, email, current_address, permanent_address = text_box_page.fill_all_fields()
+            output_name, output_email, output_curr_addr, output_per_addr = text_box_page.check_filled_form()
+            assert full_name == output_name, "the full name does not match"
+            assert email == output_email, "the email does not match"
+            assert current_address == output_curr_addr, "the current address does not match"
+            assert permanent_address == output_per_addr, "the permanent address does not match"
