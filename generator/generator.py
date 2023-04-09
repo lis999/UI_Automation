0ddaab5ae905408df8c42cbe1c9f4feb5b0005d2
Sergo Li <sergo.li.seattle@gmail.com>
2023-04-09 15:32:05 -0700

    added test web table

--- generator/generator.py
+++ generator/generator.py
@@ -1,3 +1,5 @@
+import random
+
 from data.data import Person
 from faker import Faker
 
@@ -8,6 +10,11 @@ Faker.seed()
 def generated_person():
     yield Person(
         full_name=fake.first_name() + " " + fake.last_name(),
+        firstname=fake.first_name(),
+        lastname=fake.last_name(),
+        age=random.randint(10, 80),
+        salary=random.randint(60000, 110000),
+        department=fake.job(),
         email=fake.email(),
         current_address=fake.address(),
         permanent_address=fake.address(),
--- locators/elements_page_locators.py
+++ locators/elements_page_locators.py
@@ -20,7 +20,7 @@ class CheckBoxPageLocators:
     EXPAND_ALL_BUTTON = (By.CSS_SELECTOR, "button[title='Expand all']")
     ITEM_LIST = (By.CSS_SELECTOR, "span[class='rct-title']")
     CHECKED_ITEMS = (By.CSS_SELECTOR, "svg[class='rct-icon rct-icon-check']")
-    TITLE_ITEM = ".//ancestor::span[@class='rct-text']"
+    TITLE_ITEM = ".//ancestor::span[@class='rct-text']"     # search by xpath going up to ancestor element
     OUTPUT_RESULT = (By.CSS_SELECTOR, "span[class='text-success']")
 
 
@@ -29,3 +29,20 @@ class RadioButtonPageLocators:
     IMPRESSIVE_RADIOBUTTON = (By.CSS_SELECTOR, "label[class^='custom-control'][for='impressiveRadio']")
     NO_RADIOBUTTON = (By.CSS_SELECTOR, "label[class^='custom-control'][for='noRadio']")
     OUTPUT_RESULT = (By.CSS_SELECTOR, "p span[class='text-success']")
+
+class WebTablePageLocators:
+    # add person form
+    ADD_BUTTON = (By.CSS_SELECTOR, 'button[id="addNewRecordButton"]')
+    FIRSTNAME_INPUT = (By.CSS_SELECTOR, 'input[id="firstName"]')
+    LASTNAME_INPUT = (By.CSS_SELECTOR, 'input[id="lastName"]')
+    EMAIL_INPUT = (By.CSS_SELECTOR, 'input[id="userEmail"]')
+    AGE_INPUT = (By.CSS_SELECTOR, 'input[id="age"]')
+    SALARY_INPUT = (By.CSS_SELECTOR, 'input[id="salary"]')
+    DEPARTMENT_INPUT = (By.CSS_SELECTOR, 'input[id="department"]')
+    SUBMIT = (By.CSS_SELECTOR, 'button[id="submit"]')
+
+    # table
+    FULL_PEOPLE_LIST = (By.CSS_SELECTOR, 'div[class="rt-tr-group"]')
+    SEARCH_INPUT = (By.CSS_SELECTOR, 'input[id="searchBox"]')
+    DELETE_BUTTON = (By.CSS_SELECTOR, 'span[title="Delete"]')
+    ROW_PARENT = ".//ancestor::div[@class='rt-tr-group']"
