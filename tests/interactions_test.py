ec6191588738b3ed51ff51b37fbda9d3d7939b60
Sergo Li <sergo.li.seattle@gmail.com>
2023-05-11 12:23:14 -0700

    test_resizable added

--- tests/interactions_test.py
+++ tests/interactions_test.py
@@ -1,4 +1,4 @@
-from pages.interactions_page import SortablePage, SelectablePage
+from pages.interactions_page import SortablePage, SelectablePage, ResizablePage
 
 
 class TestInteractions:
@@ -22,3 +22,14 @@ class TestInteractions:
             item_grid = selectable_page.select_grid_item()
             assert len(item_list) > 0, 'no elements were selected'
             assert len(item_grid) > 0, 'no elements were selected'
+
+    class TestResizablePage:
+
+        def test_resizable(self, driver):
+            resizable_page = ResizablePage(driver, "https://demoqa.com/resizable")
+            resizable_page.open()
+            max_box, min_box = resizable_page.change_size_resizable_box()
+            max_f, min_f = resizable_page.change_size_resizable()
+            assert ('500px', '300px') == max_box, 'maximus size not equal to 500px, 300px'
+            assert ('150px', '150px') == min_box, 'minimum size not equal to 150px, 150px'
+            assert max_f != min_f, 'resizable has not been changed'
