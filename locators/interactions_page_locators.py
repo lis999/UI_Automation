78fc38b9a80b73f1f0acf50239f797d4ac94e6e7
Sergo Li <sergo.li.seattle@gmail.com>
2023-05-10 20:33:41 -0700

    test_sortable added

--- locators/interactions_page_locators.py
+++ locators/interactions_page_locators.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 
 
-class InteractionsPageLocators:
+class SortablePageLocators:
     TAB_LIST = (By.CSS_SELECTOR, "a[id='demo-tab-list']")
     LIST_ITEM = (By.CSS_SELECTOR, "div [id='demo-tabpane-list'] div[class='list-group-item list-group-item-action']")
 
